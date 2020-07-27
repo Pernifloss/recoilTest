87c6cc5b36cb05ec3d61141bb0d2c1e2506503d4
pernifloss <pernifloss@gmail.com>
2020-07-27 13:04:17 -0300

    add grouper

---
+++ src/utils/executionGrouper.ts
@@ -0,0 +1,13 @@
+const groupByFieldscontainer = (data: any) => {
+    return data.list.items.reduce((a: any, b: any) => {
+        const found = a.find((j: any) => j.fieldscontainer === b.fieldscontainer)
+        if (found) {
+            found.executions.push(b)
+        } else {
+            a.push({executions: [b], fieldscontainer: b.fieldscontainer})
+
+        }
+        return [...a]
+    }, [])
+}
+export default groupByFieldscontainer;
