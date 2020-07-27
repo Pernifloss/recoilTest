a815ced9ce25ddbddfcf25374f9f38957f914189
pernifloss <pernifloss@gmail.com>
2020-07-27 13:18:30 -0300

    run prettier

--- src/utils/executionGrouper.test.ts
+++ src/utils/executionGrouper.test.ts
@@ -1,87 +1,91 @@
 import groupByFieldscontainer from "./executionGrouper";
 
 const data = {
-    "list": {
-        "items": [
-            {
-                "id": "365",
-                "date": "2020-07-27T14:18:49,0632Z",
-                "anonymizefield": "131",
-                "fieldscontainer": "129",
-                "sequential": "false",
-                "count": "100005",
-                "execsession": "453",
-                "time": "29439",
-                "status": "OK"
-            },
-            {
-                "id": "366",
-                "date": "2020-07-27T14:18:49,0633Z",
-                "anonymizefield": "133",
-                "fieldscontainer": "129",
-                "sequential": "false",
-                "count": "100005",
-                "execsession": "453",
-                "time": "29439",
-                "status": "OK"
-            },
-            {
-                "id": "367",
-                "date": "2020-07-27T14:19:15,0266Z",
-                "anonymizefield": "134",
-                "fieldscontainer": "105",
-                "sequential": "false",
-                "count": "100005",
-                "execsession": "453",
-                "time": "23524",
-                "status": "OK"
-            }
-        ],
-        "total": "3",
-        "count": "3",
-        "rank": "0"
-    }
-}
-it('should group by fieldsContainerId', () => {
-
-    expect(groupByFieldscontainer(data)).toEqual([{
-        fieldscontainer: '129', executions: [{
-            "id": "365",
-            "date": "2020-07-27T14:18:49,0632Z",
-            "anonymizefield": "131",
-            "fieldscontainer": "129",
-            "sequential": "false",
-            "count": "100005",
-            "execsession": "453",
-            "time": "29439",
-            "status": "OK"
+  list: {
+    items: [
+      {
+        id: "365",
+        date: "2020-07-27T14:18:49,0632Z",
+        anonymizefield: "131",
+        fieldscontainer: "129",
+        sequential: "false",
+        count: "100005",
+        execsession: "453",
+        time: "29439",
+        status: "OK"
+      },
+      {
+        id: "366",
+        date: "2020-07-27T14:18:49,0633Z",
+        anonymizefield: "133",
+        fieldscontainer: "129",
+        sequential: "false",
+        count: "100005",
+        execsession: "453",
+        time: "29439",
+        status: "OK"
+      },
+      {
+        id: "367",
+        date: "2020-07-27T14:19:15,0266Z",
+        anonymizefield: "134",
+        fieldscontainer: "105",
+        sequential: "false",
+        count: "100005",
+        execsession: "453",
+        time: "23524",
+        status: "OK"
+      }
+    ],
+    total: "3",
+    count: "3",
+    rank: "0"
+  }
+};
+it("should group by fieldsContainerId", () => {
+  expect(groupByFieldscontainer(data)).toEqual([
+    {
+      fieldscontainer: "129",
+      executions: [
+        {
+          id: "365",
+          date: "2020-07-27T14:18:49,0632Z",
+          anonymizefield: "131",
+          fieldscontainer: "129",
+          sequential: "false",
+          count: "100005",
+          execsession: "453",
+          time: "29439",
+          status: "OK"
         },
-            {
-                "id": "366",
-                "date": "2020-07-27T14:18:49,0633Z",
-                "anonymizefield": "133",
-                "fieldscontainer": "129",
-                "sequential": "false",
-                "count": "100005",
-                "execsession": "453",
-                "time": "29439",
-                "status": "OK"
-            }]
-    }, {
-        fieldscontainer: '105', executions: [{
-            "id": "367",
-            "date": "2020-07-27T14:19:15,0266Z",
-            "anonymizefield": "134",
-            "fieldscontainer": "105",
-            "sequential": "false",
-            "count": "100005",
-            "execsession": "453",
-            "time": "23524",
-            "status": "OK"
-        }]
+        {
+          id: "366",
+          date: "2020-07-27T14:18:49,0633Z",
+          anonymizefield: "133",
+          fieldscontainer: "129",
+          sequential: "false",
+          count: "100005",
+          execsession: "453",
+          time: "29439",
+          status: "OK"
+        }
+      ]
     },
-
-    ]);
-
+    {
+      fieldscontainer: "105",
+      executions: [
+        {
+          id: "367",
+          date: "2020-07-27T14:19:15,0266Z",
+          anonymizefield: "134",
+          fieldscontainer: "105",
+          sequential: "false",
+          count: "100005",
+          execsession: "453",
+          time: "23524",
+          status: "OK"
+        }
+      ]
+    }
+  ]);
 });
-
--- src/utils/executionGrouper.ts
+++ src/utils/executionGrouper.ts
@@ -1,13 +1,18 @@
 const groupByFieldscontainer = (data: any) => {
-    return data.list.items.reduce((a: any, b: any) => {
-        const found = a.find((j: any) => j.fieldscontainer === b.fieldscontainer)
-        if (found) {
-            found.executions.push(b)
-        } else {
-            a.push({executions: [b], fieldscontainer: b.fieldscontainer})
+  return data.list.items.reduce((a: any, b: any) => {
+    const found = a.find((j: any) => j.fieldscontainer === b.fieldscontainer);
+    if (found) {
+      found.executions.push(b);
+    } else {
+      a.push({
+        executions: [b],
 
-        }
-        return [...a]
-    }, [])
-}
+        fieldscontainer: b.fieldscontainer
+      });
+    }
+
+    return [...a];
+  }, []);
+};
+
 export default groupByFieldscontainer;
